cdb240079e74db3e56cb23f640f73a508afcc986
Gurkirat-s <gurkiratsingh@hotmail.ca>
2023-03-05 17:31:37 -0500

    Completed project cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Projects.scss";
 import projectsData from "../../data/projectsData";
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineWeb } from "react-icons/md";
 
 const Projects = () => {
   console.log(projectsData());
   const projects = projectsData();
   return (
-    <div className="projects">
-      <div className="container">
-        <h1>Projects</h1>
-        {projects.map((item) => {
-          return <ProjectCard project={item} />;
-        })}
+    <div id="projects" className="projects">
+      <div className="container-small">
+        <h2>Projects</h2>
+        <div className="projects-grid">
+          {projects.map((item, index) => {
+            return <ProjectCard key={index} project={item} />;
+          })}
+        </div>
       </div>
     </div>
   );
@@ -20,29 +24,47 @@ const Projects = () => {
 const ProjectCard = ({ project }) => {
   return (
     <div className="project-card">
-      {project.imgURL && (
-        <img src={project.imgURL} alt="Screenshot of project" />
-      )}
+      <div className="img-container">
+        {project.imgURL && (
+          <img src={project.imgURL} alt="Screenshot of project" />
+        )}
+      </div>
 
       <div className="card-info">
-        <h2>{project.title}</h2>
+        <h3>{project.title}</h3>
+        {project.type && <h4>{project.type}</h4>}
         <p>{project.description}</p>
-        <div>
-          {project.type && <h3>{project.type}</h3>}
-          {project.demoURL && <a href="#about">{project.demoURL}</a>}
-          {project.githubURL && <a href="#about">{project.githubURL}</a>}
+        <ul className="tools">
+          {project.tools.map((item) => {
+            return <li>{item}</li>;
+          })}
+        </ul>
+
+        <div className="buttons">
+          {project.demoURL && <ButtonDemo url={project.demoURL} />}
+          {project.githubURL && <ButtonGithub url={project.githubURL} />}
         </div>
       </div>
     </div>
   );
 };
 
-const ButtonGithub = () => {
-  return <div></div>;
+const ButtonGithub = ({ url }) => {
+  return (
+    <a className="btn btn-github" href={url}>
+      <FaGithub />
+      <span className="btn-text">Code</span>
+    </a>
+  );
 };
 
-const ButtonDemo = () => {
-  return <div></div>;
+const ButtonDemo = ({ url }) => {
+  return (
+    <a className="btn btn-demo" href={url}>
+      <MdOutlineWeb className="flex-item" />
+      <span className="btn-text flex-item">Demo</span>
+    </a>
+  );
 };
 
 export default Projects;
